8ea2d0c4ec22380410d61e90f095fb0430d52a30
Travis Kopp <teakopp@github.com>
2018-03-13 16:47:15 -0700

    Update create-database.js

--- server/create-database.js
+++ server/create-database.js
@@ -2,7 +2,7 @@ const MongoClient = require('mongodb').MongoClient;
 let url = "mongodb://localhost:27017/atomic"
 database = "atomic"
 collection = "players"
-data = {some : "data }
+data = {some : "data" }
 
 MongoClient.connect(url, (err, db)=>{
   if (err) throw err;
