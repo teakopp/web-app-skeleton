9effa12412c41280d90c21f1c6c75f4809f5b392
Travis Kopp <teakopp@github.com>
2018-03-13 16:46:54 -0700

    removed Atomic data

--- server/create-database.js
+++ server/create-database.js
@@ -2,24 +2,7 @@ const MongoClient = require('mongodb').MongoClient;
 let url = "mongodb://localhost:27017/atomic"
 database = "atomic"
 collection = "players"
-data = {
-        name : "name",
-        charactersheet : {
-          description : "description",
-          refresh : "refresh",
-          highconcept : "high concept",
-          trouble : "trouble",
-          aspects : ["1", "2", "3"],
-          skills : {"Athletics" : 3, "Fight" : 4 },
-          extras : "???",
-          stunts : "???",
-          physicalstress : 0,
-          mentalstress : 0,
-          consequences : []
-        },
-        items : [],
-        notes : []
-      }
+data = {some : "data }
 
 MongoClient.connect(url, (err, db)=>{
   if (err) throw err;
